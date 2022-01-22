029d6d427e0f2afdf1fa5bf3a8c960402df235ab
Harry Dean Hudson <deanhudson@gmail.com>
2022-01-22 10:40:43 +0100

    Missed this file

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import { Entity } from "./entity";
 import { GameLoop } from "./gameLoop";
 import { Point } from "./point";
-import { RedBox } from "./rect/RedBox";
+import { RedBox } from "./painter/RedBox";
 
 const cvs  = <HTMLCanvasElement>document.getElementById("canvas");
 const loop = new GameLoop(cvs);
 
 let pos  = new Point(100, 100)
-let rect = new RedBox(40, 40);
+let painter= new RedBox(40, 40);
 
-let ent = new Entity(rect, pos, (time, e) => {
+let ent = new Entity(painter, pos, (time, e) => {
     let {x, y} = e.position;
     e.position = new Point(x + 1, y - 1);
 });
